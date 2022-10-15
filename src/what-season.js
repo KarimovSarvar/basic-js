036eb7e915ec9362f95fa41a564ea14afca5da0f
Sarvar <ksz95@bk.ru>
2022-10-15 20:33:03 +0600

    feat: made what season

--- src/what-season.js
+++ src/what-season.js
@@ -12,19 +12,28 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
  function getSeason(date) {
-  let month = date.getMonth();
-  if (month === 11 || month === 0 || month === 1) {
-    return 'winter';
-  }
-  else if (month === 2 || month === 3 || month === 4) {
-    return 'spring'
-  }
-  else if (month === 5 || month === 6 || month === 7) {
-    return 'summer'
-  }
-  else if (month === 8 || month === 9 || month === 10) {
-    return 'autumn'
-  }
+  if(typeof date == 'object') {
+    let month = date.getMonth();
+    let day = date.getDate();
+    if (month === 11 || month === 0 || month === 1) {
+      return 'winter';
+    }
+    else if (month === 2 || month === 3 || month === 4) {
+      return 'spring'
+    }
+    else if (month === 5 || month === 6 || month === 7) {
+      return 'summer'
+    }
+    else if (month === 8 || month === 9 || month === 10) {
+      return 'autumn'
+    }
+    else if (month > 11) {
+      return "Invalid date!"
+    }
+    else if (day > 31) {
+      return "Invalid date!"
+    }
+  } else return 'Unable to determine the time of year!'
 }
 
 module.exports = {
