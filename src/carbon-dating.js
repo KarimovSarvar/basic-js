8bab20f55aa7fc035e7300e3f4ac7c4cadaebc93
Sarvar <ksz95@bk.ru>
2022-10-17 13:26:30 +0600

    feat: made transform

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,24 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+  if (typeof sampleActivity === 'string') {
+    if (!isNaN(sampleActivity)) {
+      let result;
+      let up =  Math.log(15/sampleActivity);
+      let k = 0.693/5730;
+      result = Math.ceil(up/k);
+      if(result < 1 || result == Infinity) {
+        return false
+      } else return result
+  }
+  else if(sampleActivity < 1) {
+    return false
+  } else return false
+} 
+  else if (arguments.length == 0) {
+    return false
+} else return false
 }
 
 module.exports = {
